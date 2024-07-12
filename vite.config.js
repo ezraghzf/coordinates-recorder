323f40883854bc881ebe0901c8f20f29dd210bab
Ezra G <ezraghzf@gmail.com>
2024-07-12 15:29:45 +0700

    Update config

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,15 @@ import fs from 'fs';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem'))
+export default defineConfig(({command}) => {
+  const config = {plugins: [vue()],}
+  if (command == 'serve') {
+    config.server = {
+      https: {
+        key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
+        cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem'))
+      }
     }
   }
+  return config;
 })
